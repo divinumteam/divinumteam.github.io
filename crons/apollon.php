1aaa08b07f7d8c945e0686f14b461feb19ae66df
divinumteam <114397050+divinumteam@users.noreply.github.com>
2022-09-26 12:50:47 +0300

    Add files via upload

---
+++ crons/apollon.php
@@ -0,0 +1,21 @@
+<br><br><br><br>
+<center><img src="https://ih0.redbubble.net/image.622119816.0212/mp,550x550,matte,ffffff,t.3u1.jpg"><br>
+</center>
+
+<!-- APOLLON TEST CRON 1 -->
+<iframe src="/crons/providers.php" scrolling="no" frameborder="0" align="center" height = "2" width = "2" name="test" border="0">
+</iframe>
+<!-- APOLLON TEST CRON 2 -->
+<iframe src="/crons/checkToAPI.php" scrolling="no" frameborder="0" align="center" height = "2" width = "2" name="test" border="0">
+</iframe>
+<!-- APOLLON TEST CRON 3 -->
+<iframe src="/crons/dripfeed.php" scrolling="no" frameborder="0" align="center" height = "2" width = "2" name="test" border="0">
+</iframe>
+<!-- APOLLON TEST CRON 4 -->
+<iframe src="/crons/autolike.php" scrolling="no" frameborder="0" align="center" height = "2" width = "2" name="test" border="0">
+</iframe>
+<!-- APOLLON TEST CRON 5 -->
+<iframe src="/crons/orders.php" scrolling="no" frameborder="0" align="center" height = "2" width = "2" name="test" border="0">
+</iframe>
+
+<!-- APOLLON PROJECT SUCCESSFUL -->
